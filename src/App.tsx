d07d92046104db2d3589d8ce3951c8d783802060
Arthur Yan <arthuryan08@gmail.com>
2021-10-07 18:54:32 -0300

    types and example data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import * as C from './App.styles'
+import { useState, useEffect } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { categories } from './data/categories'
+import { items } from './data/items'
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter'
+import { TableArea } from './components/TableArea'
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([])
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(()=>{
+    setFilteredList(filterListByMonth(list, currentMonth))
+  }, [list, currentMonth]);
+
   return (
     <C.Container>
       <C.Header>
         <C.HeaderText>Sitema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        ...
+        
+        {/* Area de informações */}
+
+        {/* Area de inserção */}
+
+        <TableArea />
+
       </C.Body>
     </C.Container>
   )
---
+++ src/components/TableArea/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Table = styled.table`
+  width: 100%;
+  background-color: #FFF;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #CCC;
+`;
+
+export const TableHeadColumn = styled.th``;
