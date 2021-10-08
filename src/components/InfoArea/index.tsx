607e7f349da64df2eee7122a21d48e5d665ff878
Arthur Yan <arthuryan08@gmail.com>
2021-10-07 21:39:27 -0300

    Info Area done
    info area with calculation function done, colors and more

--- src/components/InfoArea/index.tsx
+++ src/components/InfoArea/index.tsx
@@ -1,20 +1,28 @@
-import * as C from './styles'
-import { formatCurrentMonth } from '../../helpers/dateFilter'
+import * as C from "./styles";
+import { formatCurrentMonth } from "../../helpers/dateFilter";
+import { ResumeItem } from '../ResumeItem'
 
 type Props = {
   currentMonth: string;
-}
+  onMonthChange: (newMonth: string) => void;
+  income: number;
+  expense: number;
+};
 
-export const InfoArea = ({ currentMonth }: Props) => {
-    const handlePrevMonth = () => {
-      let [year, month] = currentMonth.split('-')
-      let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1)
-      currentDate.setMonth( currentDate.getMonth() - 1 )
-    }
+export const InfoArea = ({ currentMonth, onMonthChange, income, expense }: Props) => {
+  const handlePrevMonth = () => {
+    let [year, month] = currentMonth.split("-");
+    let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1);
+    currentDate.setMonth(currentDate.getMonth() - 1);
+    onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`);
+  };
 
-    const handleNextMonth = () => {
-
-    }
+  const handleNextMonth = () => {
+    let [year, month] = currentMonth.split("-");
+    let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1);
+    currentDate.setMonth(currentDate.getMonth() + 1);
+    onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`);
+  };
 
   return (
     <C.Container>
@@ -24,8 +32,14 @@ export const InfoArea = ({ currentMonth }: Props) => {
         <C.MonthArrow onClick={handleNextMonth}>➡️</C.MonthArrow>
       </C.MonthArea>
       <C.ResumeArea>
-
+        <ResumeItem title='Receitas' value={income} />
+        <ResumeItem title='Despesas' value ={expense} />
+        <ResumeItem 
+          title='Balanço' 
+          value={income - expense}
+          color={(income - expense) < 0 ? 'red' : 'green'}
+        />
       </C.ResumeArea>
     </C.Container>
-  )
-}
+  );
+};
